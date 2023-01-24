672dcc0198321e5eed35d72cff9ede29c786cf5c
Training <training@ip-172-31-19-102.ec2.internal>
2023-01-24 18:22:01 +0000

    Fix issue with syntax

--- Capstone Project/accounts/repositories/address.py
+++ Capstone Project/accounts/repositories/address.py
@@ -7,20 +7,18 @@ class AddressRepository():
     DB_PASS = "password123"
     DB_HOST = "localhost"
     DB_PORT = "5432"
-    try:
-        conn = psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
-        print("Connected to DB")
-    except:
-        print("Unable to connect to db")
+
+    conn = psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
+    print("Connected to DB")
 
     #Create cursor to allow for query execution
     cursor = conn.cursor()
 
     def insert(self, address: Address):
-        cursor.execute('INSERT INTO Address (id, address, city, state, zipcode) \
+        self.cursor.execute('INSERT INTO Address (id, address, city, state, zipcode) \
         VALUES (?, ?, ?, ?, ?)', [address.id, address.address, address.city, address.state, address.zip_code])
-        conn.commit()
-        conn.close()
+        self.conn.commit()
+        self.conn.close()
 
     def get_by_id(self, id):
         pass
