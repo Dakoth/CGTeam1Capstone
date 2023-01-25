93865deb13557bf6b1e0b20924741b2cebc348e8
Training <training@ip-172-31-19-102.ec2.internal>
2023-01-25 18:02:14 +0000

    Fixed issue with connection statements in address repo

--- Capstone Project/accounts/repositories/address.py
+++ Capstone Project/accounts/repositories/address.py
@@ -19,35 +19,35 @@ class AddressRepository():
                 return address
 
     def get_by_id(self, id):
-        conn = psycopg2.connect(host=self.DB_HOST, database=self.DB_NAME, user=self.DB_USER, password=self.DB_PASS)
-        cursor = conn.cursor()
-        cursor.execute("""SELECT * FROM public."Address" WHERE id=%(id)s;""", {'id': id})
-        row = cursor.fetchone()
-        return Address(id=row[0], address=row[1], city=row[2], state=row[3], zip_code=row[4])
+        with psycopg2.connect(host=self.DB_HOST, database=self.DB_NAME, user=self.DB_USER, password=self.DB_PASS) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("""SELECT * FROM public."Address" WHERE id=%(id)s;""", {'id': id})
+                row = cursor.fetchone()
+                return Address(id=row[0], address=row[1], city=row[2], state=row[3], zip_code=row[4])
     
     def get_all(self):
         results = []
-        conn = psycopg2.connect(host=self.DB_HOST, database=self.DB_NAME, user=self.DB_USER, password=self.DB_PASS)
-        cursor = conn.cursor()
-        cursor.execute("""
-        SELECT * FROM public."Address"
-        """)
-        rows = cursor.fetchall()
-        for row in rows:
-            results.append(Address(id=row[0], address=row[1], city=row[2], state=row[3], zip_code=row[4]))
-        return results
+        with psycopg2.connect(host=self.DB_HOST, database=self.DB_NAME, user=self.DB_USER, password=self.DB_PASS) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("""
+                SELECT * FROM public."Address"
+                """)
+                rows = cursor.fetchall()
+                for row in rows:
+                    results.append(Address(id=row[0], address=row[1], city=row[2], state=row[3], zip_code=row[4]))
+                return results
     
     def update(self, address: Address):
-        conn = psycopg2.connect(host=self.DB_HOST, database=self.DB_NAME, user=self.DB_USER, password=self.DB_PASS)
-        cursor = conn.cursor()
-        cursor.execute("""
-        UPDATE public."Address" SET address=%(address)s, city=%(city)s, state=%(state)s, zip_code=%(zip)s WHERE id=%(id)s
-        """, {'id': address.id, 'address': address.address, 'city': address.city, 'state': address.state, 'zip': address.zip_code})
-        return self.get_by_id(address.id)
+        with psycopg2.connect(host=self.DB_HOST, database=self.DB_NAME, user=self.DB_USER, password=self.DB_PASS) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("""
+                UPDATE public."Address" SET address=%(address)s, city=%(city)s, state=%(state)s, zip_code=%(zip)s WHERE id=%(id)s
+                """, {'id': address.id, 'address': address.address, 'city': address.city, 'state': address.state, 'zip': address.zip_code})
+                return self.get_by_id(address.id)
     
     def delete(self, id):
-        conn = psycopg2.connect(host=self.DB_HOST, database=self.DB_NAME, user=self.DB_USER, password=self.DB_PASS)
-        cursor = conn.cursor()
-        cursor.execute("""
-        DELETE FROM public."Address" WHERE id=%(id)s
-        """, {'id': id})
+        with psycopg2.connect(host=self.DB_HOST, database=self.DB_NAME, user=self.DB_USER, password=self.DB_PASS) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("""
+                DELETE FROM public."Address" WHERE id=%(id)s
+                """, {'id': id})
